09c113a8c0d265160ce01e685d23c91eb642da09
dbjerry <kimjitae91@gmail.com>
2019-04-08 23:58:17 +0900

    2019-04-08 23:58 update weather

--- weather.js
+++ weather.js
@@ -1,5 +1,20 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = 'cc49393d7ce86cef0fe229935573ce2f';
 const COORDS = 'coords';
+
+function getWeather(lat, lon){
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    const temperature = json.main.temp;
+    const place = json.name;
+    weather.innerText = `${temperature}℃ @${place}`;
+  });
+}
 
 function saveCoords(coordsObj){
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +28,7 @@ function handleGeoSuccess(position){
     longitude
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -28,7 +44,8 @@ function loadCoords(){
   if(loadedCoords === null){
     askForCoords();
   } else {
-
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
